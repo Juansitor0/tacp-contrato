c5540a4f6a37cece9247025d2f31582f273e222f
AlyssonJuan <souzajuan407@gmai.com>
2026-01-19 16:57:15 -0300

    Enable Firebase Cloud Messaging in service worker

--- sw.js
+++ sw.js
@@ -1,10 +1,26 @@
-self.addEventListener('push', function(event) {
-    const data = event.data.json();
-    const options = {
-        body: data.body,
-        icon: 'https://cdn-icons-png.flaticon.com/512/1182/1182761.png',
-        badge: 'https://cdn-icons-png.flaticon.com/512/1182/1182761.png',
-        vibrate: [200, 100, 200]
-    };
-    event.waitUntil(self.registration.showNotification(data.title, options));
+importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDeXp1gHm2cyd0MPRbyj7NMm0HlnlI9tCE",
+    authDomain: "contrato-online.firebaseapp.com",
+    databaseURL: "https://contrato-online-default-rtdb.firebaseio.com",
+    projectId: "contrato-online",
+    storageBucket: "contrato-online.firebasestorage.app",
+    messagingSenderId: "707541071284",
+    appId: "1:707541071284:web:6bc61a769477e24feeaee1"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: 'https://cdn-icons-png.flaticon.com/512/1182/1182761.png',
+    badge: 'https://cdn-icons-png.flaticon.com/512/1182/1182761.png',
+    vibrate: [200, 100, 200]
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
