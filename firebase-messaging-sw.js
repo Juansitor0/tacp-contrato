610115cf2cf6a09ca3b7016691d77db01fc4cd8b
Juan_Carlos965 <160427321+Juansitor0@users.noreply.github.com>
2026-01-19 16:38:45 -0300

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,28 @@
+importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDeXp1gHm2cyd0MPRbyj7NMm0HlnlI9tCE",
+    authDomain: "contrato-online.firebaseapp.com",
+    databaseURL: "https://contrato-online-default-rtdb.firebaseio.com",
+    projectId: "contrato-online",
+    storageBucket: "contrato-online.firebasestorage.app",
+    messagingSenderId: "707541071284",
+    appId: "1:707541071284:web:6bc61a769477e24feeaee1"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(payload => {
+  console.log('[firebase-messaging-sw.js] Push recebido', payload);
+
+  self.registration.showNotification(
+    payload.notification.title,
+    {
+      body: payload.notification.body,
+      icon: 'https://cdn-icons-png.flaticon.com/512/1182/1182761.png',
+      badge: 'https://cdn-icons-png.flaticon.com/512/1182/1182761.png',
+      vibrate: [200, 100, 200]
+    }
+  );
+});
